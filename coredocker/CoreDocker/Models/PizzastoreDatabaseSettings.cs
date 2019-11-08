62e639005afe15b8cb209c2684ab64c88279d00c
lucas.valotto <val-oto@hotmail.com>
2019-11-08 16:41:18 -0300

    Atualziacao banco de dados

---
+++ coredocker/CoreDocker/Models/PizzastoreDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDocker.Models
+{
+        public class PizzastoreDatabaseSettings : IPizzastoreDatabaseSettings
+        {
+            public string BooksCollectionName { get; set; }
+            public string ConnectionString { get; set; }
+            public string DatabaseName { get; set; }
+        }
+
+        public interface IPizzastoreDatabaseSettings
+        {
+            string BooksCollectionName { get; set; }
+            string ConnectionString { get; set; }
+            string DatabaseName { get; set; }
+        }
+
+}
